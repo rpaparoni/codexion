04b0482c207dacddd4baec7b7a83c3f72b5ca574
rpaparoni <paparoniricardo@gmail.com>
2026-08-11 17:08:33 +0200

    parsing

--- coders/includes/codexion.h
+++ coders/includes/codexion.h
@@ -6,15 +6,16 @@
 /*   By: ripaparo <ripaparo@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 13:48:27 by ripaparo          #+#    #+#             */
-/*   Updated: 2026/08/11 13:53:32 by ripaparo         ###   ########.fr       */
+/*   Updated: 2026/08/11 16:54:32 by ripaparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-#ifndef
-#define
+#ifndef CODEXION_H
+# define CODEXION_H
 
-int	main(int argc, char *argv[])
+int     main(int argc, char *argv[]);
+char**  parsing(char *argv[]);
 
 #endif
--- coders/src/main.c
+++ coders/src/main.c
@@ -6,13 +6,16 @@
 /*   By: ripaparo <ripaparo@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 13:26:45 by ripaparo          #+#    #+#             */
-/*   Updated: 2026/08/11 14:29:28 by ripaparo         ###   ########.fr       */
+/*   Updated: 2026/08/11 16:53:27 by ripaparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <coders/includes/codexion.h>
+#include "../includes/codexion.h"
 
 int	main(int argc, char *argv[])
 {
-	
+    if (argc == 7)
+	    parsing(argv);
+    else
+        printf("error");
 }
